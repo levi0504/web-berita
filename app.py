c2404fccb9686cfd129b16aa3cb84a0f0f7aad42
levi0504 <skymoonteams@gmail.com>
2025-11-11 22:20:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -589,5 +589,4 @@ def init_db():
 # RUN APP
 # -----------------------------------
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
